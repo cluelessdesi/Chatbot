bf9b19bac36f8e1008c1117acb5e56df82eeeed8
cluelessdesi <sufiyanretreat@gmail.com>
2025-10-01 19:10:18 -0500

    fixed api issue and model issue, should work hopefully

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
         { role: "system", content: systemPrompt },
         { role: "user", content: message }
       ],
-      model: "llama3-70b-8192",
+      model: "llama-3.1-8b-instant",
       temperature: 1,
       max_tokens: 1024,
       top_p: 1,
@@ -43,69 +43,3 @@ export async function POST(req) {
       }
     );
 }
-
-// export async function POST() {
-//   const chatCompletion = await getGroqChatCompletion();
-//   // Print the completion returned by the LLM.
-//   console.log(chatCompletion.choices[0]?.message?.content || "whyyyy");
-//     //   return new NextResponse()
-// }
-
-// export async function getGroqChatCompletion() {
-//     return groq.chat.completions.create({
-//       messages: [
-//         {
-//           role: "user",
-//           content: systemPrompt,
-//         },
-//       ],
-//       model: "llama3-8b-8192",
-//     }).then((response) => response.choices[0].message.content);
-// }
-
-// import { NextResponse } from "next/server";
-// import OpenAI from "openai";
-
-// const systemPrompt = "creating an ai powered chatbot for a headtstarter AI project"
-
-// export async function POST(request) {
-//     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
-//     const data = await request.json()
-    
-//     const completion = await openai.chat.completions.create({
-//         messages : [
-//             {
-//                 role : 'system',
-//                 content : systemPrompt
-//             },
-//             ...data,
-//         ], 
-//         model : 'gpt-4o-mini',
-//         stream : true,
-//     })
-
-    // const stream = new ReadableStream({
-    //     async start (controller) {
-    //         const encoder = new TextEncoder()
-    //         try {
-    //             for await (const chunk of completion) {
-    //                 const content = chunk.choices[0]?.delta?.content
-    //                 if (content) {
-    //                     const text = encoder.encode(content)
-    //                     controller.enqueue(text)
-    //                 }
-    //             }
-    //         }
-    //         catch (err) {
-    //             controller.error(err)
-    //         }
-    //         finally {
-    //             controller.close()
-    //         }
-    //     }
-    // })
-
-    // return new NextResponse(stream)
-
-// }
-
